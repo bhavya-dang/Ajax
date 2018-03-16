7f1df747197fe54a53f1e0ba9e1d586267bf33fe
Bhavya Dang <37131433+TritaxGamer@users.noreply.github.com>
2018-03-16 18:22:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,3 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-
 const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
 const fs = require("fs");
