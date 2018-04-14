096f9daf1cd3933f0432b826d253c789bd121596
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-14 19:37:20 +0530

    Update quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -30,21 +30,21 @@ const options = {
   errors: ["time"],
 };
 
-exports.run = async (client, message, args, tools) => {
+module.exports.run = async (bot, message, args) => {
   
   const item = quiz[Math.floor(Math.random() * quiz.length)];
   await message.channel.send(item.q);
   try {
     const collected = await message.channel.awaitMessages(answer => item.a.includes(answer.content.toLowerCase()), options);
     const winnerMessage = collected.first();
-    return message.channel.send({embed: new Discord.MessageEmbed()
-                                 .setAuthor(`Winner: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL())
+    return message.channel.send({embed: new Discord.RichEmbed()
+                                 .setAuthor(`Winner: ${winnerMessage.author.tag}`, winnerMessage.author.displayAvatarURL)
                                  .setTitle(`Correct Answer: \`${winnerMessage.content}\``)
                                  .setFooter(`Question: ${item.q}`)
-                                 .setColor('GREEN')
+                                 .setColor(message.guild.me.displayHexColor)
                                 })
   } catch (_) {
-    return message.channel.send({embed: new Discord.MessageEmbed()
+    return message.channel.send({embed: new Discord.RichEmbed()
                                  .setAuthor('No one got the answer in time!')
                                  .setTitle(`Correct Answer(s): \`${item.a}\``)
                                  .setFooter(`Question: ${item.q}`)
