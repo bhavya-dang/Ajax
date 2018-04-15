c90cd53e46487631a29c42067e9e65d6f2b3d51a
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-15 11:53:12 +0530

    Update hb.js

--- commands/hb.js
+++ commands/hb.js
@@ -2,7 +2,7 @@ const hastebin = require('hastebin-gen');
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-let code = args.slice(1).join(" ")
+let code = args.join(" ")
 hastebin(code, "js").then(r => {
     message.channel.send(r); //https://hastebin.com/someurl.js
 }).catch(console.error);
