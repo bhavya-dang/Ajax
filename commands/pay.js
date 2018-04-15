b4b18a41ca96a1031c17e639aa4c2100d305e669
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-15 11:38:37 +0530

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -1,28 +1,29 @@
-const Discord = require('discord.js');
-const db = require('quick.db')
+const Discord = require('discord.js'),
+      db = require('quick.db');
 
-module.exports.run = async (bot, message, args) => {
-  if (!message.mentions.members.first()) return message.channel.send('**Please mention a user!**')
+exports.run = async (client, message, args, tools) => {
 
-  let targetMember = message.mentions.members.first(),
-    amount = parseInt(args.join(' ').replace(targetMember, ''))
+    if (!message.mentions.members.first()) return tools.embed(message.channel, '**Please mention a user!**');
 
-  if (isNaN(amount)) return message.channel.send('**Please define an amount!**')
+    let targetMember = message.mentions.members.first(),
+        amount = parseInt(args.join(' ').replace(targetMember, ''));
+    
+    if (isNaN(amount)) return message.channel.send('**Please define an amount!**');
 
-  let targetBalance = await db.fetch(`userBalance_${targetMember.id}`),
-    selfBalance = await db.fetch(`userBalance_${message.author.id}`),
-    startBalance = 0 // Starting Balance
+    let targetBalance = await db.fetch(`userBalance_${targetMember.id}`),
+        selfBalance = await db.fetch(`userBalance_${message.author.id}`),
+        startBalance = 0; // Starting Balance
 
-  if (targetBalance === null) targetBalance = startBalance
-  if (selfBalance === null) selfBalance = startBalance
+    if (targetBalance === null) targetBalance = startBalance;
+    if (selfBalance === null) selfBalance = startBalance;
 
-  if (amount > selfBalance) return message.channel.send('**Sorry you don\'t have enough money.**')
+    if (amount > selfBalance) return tools.embed(message.channel, '**Sorry you don\'t have enough money.**');
 
-  db.add(`userBalance_${targetMember.id}`, amount)
-  db.subtract(`userBalance_${message.author.id}`, amount)
+    db.add(`userBalance_${targetMember.id}`, amount);
+    db.subtract(`userBalance_${message.author.id}`, amount);
 
- message.channel.send(`**Successfully given $${amount} to ${targetMember.user.username}!**`)
+   message.channel.send(`**You payed $${amount} to ${targetMember.user.username}!**`);
 }
-module.exports.help = {
-  name: "pay"
+module.exports.help ={
+    name: "pay"
 }
