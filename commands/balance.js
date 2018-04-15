4dc64c16410dd437529c9b8df3a72c5f65f80fc9
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-15 11:40:58 +0530

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js'),
       db = require('quick.db');
 
-exports.run = async (client, message, args, tools) => {
+exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || message.author;
     let balance = await db.fetch(`userBalance_${user.id}`);
     const embed = new Discord.RichEmbed()
