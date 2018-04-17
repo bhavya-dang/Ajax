89b30914e58b8f45d88a8fe6ef7199d0fcc10a52
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-17 16:43:25 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,13 @@
 	message.channel.send(embed)
 	}
     
+	let prefix = db.fetch(`prefix_${message.guild.id}`).then(i => {
+	if(!i){
 	let prefix = botconfig.prefix;
+	}else{
+	prefix = i;
+	}
+	})
 	let args = message.content.slice(prefix.length).trim().split(" ");
 	let cmd = args.shift().toLowerCase();
 	if(message.author.bot) return undefined;;
