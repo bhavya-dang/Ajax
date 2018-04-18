f6f0e08933f70f3cf1e85ae392e011ebc0ed93b9
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-18 20:25:31 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!bUser) return message.channel.send("Can't find user!");
     let bReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("No can do pal!");
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("You don't have `MANAGE_MEMBERS` permissons.");
     if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("That person can't be kicked!");
 
     let banEmbed = new Discord.RichEmbed()
@@ -16,14 +16,14 @@ module.exports.run = async (bot, message, args) => {
     .addField("Reason", bReason);
 
     let incidentchannel = message.guild.channels.find(`name`, "mod-log");
-    if(!incidentchannel) return message.channel.send("Can't findchannel.");
+    if(!incidentchannel) return message.channel.send("Can't find channel.");
     let embed = new Discord.RichEmbed()
     .setTitle("BAN")
     .addField("Banned In", message.guild)
     .addField("Moderator", `${message.author.tag}`)
     .addField("Reason", bReason)
     bUser.send(embed);
-  bUser.ban(bReason);
+  bUser.ban(bReason).then(msg => msg.channel.send(":tickYes: | That member has been banned."))
   incidentchannel.send(banEmbed);
 }
 
