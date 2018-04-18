fb076d1a942bdd36094145ba8a256c65348aca28
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-18 20:42:03 +0530

    Update setgame.js

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,6 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
+  if(message.author.id !== '414111663076147201')
   let GameTypes = ["WATCHING", "PLAYING", "LISTENING"];
   let GameType = args[0];
   let GameName = args.slice(1).join(' ');
