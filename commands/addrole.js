2cb0cecef1e2dd32d91b870a22b0816bcaac454b
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-18 21:08:57 +0530

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You dont have `MANAGE_MEMBERS` permissions.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("<:tickNo:432418492667396097> | You dont have `MANAGE_MEMBERS` permissions.");
   let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
   if(!rMember) return message.reply("That user cannot be found!");
   let role = args.join(" ").slice(22);
@@ -10,7 +10,7 @@ module.exports.run = async (bot, message, args) => {
   let gRole = message.guild.roles.find(`name`, role);
   if(!gRole) return message.reply("Couldn't find that role.");
 
-  if(rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  if(rMember.roles.has(gRole.id)) return message.reply("<:tickNo:432418492667396097> | They already have that role.");
   await(rMember.addRole(gRole.id));
 
   try{
