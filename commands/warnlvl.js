a9c024dd239b65723431b22f950355b61d662436
Bhavya Dang <37131433+TritaxXCoder@users.noreply.github.com>
2018-04-18 20:58:50 +0530

    Update warnlvl.js

--- commands/warnlvl.js
+++ commands/warnlvl.js
@@ -1,16 +1,21 @@
 const Discord = require("discord.js");
 const fs = require("fs");
-const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async (bot, message, args) => {
 
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("You can't do that.");
+  if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("You don't have `MANGE_MEMBERS` permissions.");
   let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
-  if(!wUser) return message.reply("Couldn't find them yo");
+  if(!wUser) return message.reply("Couldn't find the user.");
   let warnlevel = warns[wUser.id].warns;
 
-  message.reply(`<@${wUser.id}> has ${warnlevel} warnings.`);
+  let embed = new Discord.RichEmbed()
+  .setTitle("Warnings")
+  .addField("User", `${wUser.user.username}`)
+  .addField("Moderator", `${message.author}`)
+  .addField("Number of Warnings", `${warnlevel}`)
+  .setColor("#f4b342")
+  message.channel.send(embed);
 
 }
 
